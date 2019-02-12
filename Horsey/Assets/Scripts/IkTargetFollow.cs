5ef1a02fb437abeb8c930243bb7d82eff49e1327
unknown <hjorturjohann94@gmail.com>
2019-02-12 19:05:10 +0000

    HJ Horse implementation can now stand, kind of

---
+++ Horsey/Assets/Scripts/IkTargetFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IkTargetFollow : MonoBehaviour
+{
+    public Transform toFollow;
+
+    void LateUpdate()
+    {
+        transform.position = toFollow.position;
+    }
+}
---
+++ Horsey/Assets/Scripts/Muscle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muscle : MonoBehaviour {
+    public float musclePower;
+
+    private float _localRot;
+    private Rigidbody2D _rb;
+
+
+    private void Start() {
+        _localRot = transform.localRotation.eulerAngles.z;
+        _rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate() {
+        float tempRot = _localRot;
+        var angleForce = Mathf.DeltaAngle(transform.localRotation.eulerAngles.z, tempRot);
+        var angleDirection = Mathf.Clamp(angleForce, -1f, 1f);
+        _rb.AddTorque(angleDirection * musclePower, ForceMode2D.Force);
+    }
+}
