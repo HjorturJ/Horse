dcc3dac29c79e82a82b1aa2180563ac290d738fe
Anna Lára <annasig95@gmail.com>
2019-02-12 15:30:00 +0000

    Scene manager funtionality

---
+++ Horsey/Assets/Scripts/Reset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("r"))
+        {
+            //load the correct scene..
+        }
+        if(Input.GetKey("q"))
+        {
+            Application.Quit();
+        }
+
+    }
+}
