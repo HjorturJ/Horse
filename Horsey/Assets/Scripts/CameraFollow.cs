896b65f3f41db5d5120098efb04800b2a27933b1
unknown <hjorturjohann94@gmail.com>
2019-02-13 18:34:28 +0000

    Camera follow script

---
+++ Horsey/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform toFollow;
+
+    void Start() {
+
+    }
+
+    void LateUpdate() {
+        transform.position = new Vector3(toFollow.position.x, transform.position.y, transform.position.z);
+    }
+}
