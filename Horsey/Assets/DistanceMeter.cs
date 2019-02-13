54c5539a7eae2a77f31c8193649b7f26007a5fe3
irisdogg <iriss16@ru.is>
2019-02-13 14:10:54 +0000

    added a countdown timer and a distance meter

---
+++ Horsey/Assets/DistanceMeter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceMeter : MonoBehaviour
+{
+    public GameObject horsePos;
+    public Slider slider;
+
+    private void Update()
+    {
+        slider.value = horsePos.transform.position.x;
+        Debug.Log("Slider value: " + slider.value);
+        Debug.Log("Horse pos: " + horsePos.transform.position.x);
+    }
+}
---
+++ Horsey/Assets/Scripts/Timer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI countDownText;
+    private float time = 120;
+    private bool timerStart = true;
+    public float restartDelay = 0.1f;
+
+    private void Start()
+    {
+        if(timerStart == true)
+        {
+            StartCountDownTimer();
+        }
+    }
+
+    void StartCountDownTimer()
+    {
+        if(countDownText != null)
+        {   
+            time = 120;
+            countDownText.text = "02:00";
+            InvokeRepeating("UpdateTimer", 0.0f, 0.01067f);
+        }
+    }
+
+    void UpdateTimer()
+    {
+        if (countDownText != null)
+        {
+            time -= Time.deltaTime;
+            string minutes = Mathf.Floor(time / 60).ToString("00");
+            string seconds = (time % 60).ToString("00");
+            countDownText.text = (minutes + ":" + seconds);
+            if (time < 0.0)
+            {
+                countDownText.text = "00:00";
+                Invoke("Restart", restartDelay);
+            }
+        }
+
+    }
+    void Restart()
+    {
+        Debug.Log("Game over!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
