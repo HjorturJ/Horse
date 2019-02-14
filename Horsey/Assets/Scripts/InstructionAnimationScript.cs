d340bc67c938c569c4fee85f17a5450e43c5383c
unknown <hjorturjohann94@gmail.com>
2019-02-14 15:25:43 +0000

    Instruction animation done in private scene

--- Horsey/Assets/Scripts/InstructionAnimationScript.cs
+++ Horsey/Assets/Scripts/InstructionAnimationScript.cs
@@ -18,16 +18,19 @@ public class InstructionAnimationScript : MonoBehaviour {
     IEnumerator ToggleAnimation() {
         var i = 1;
         var k = 6;
+        float speed = 0.5f;
         while (true) {
             if (i > 3) i = 1;
             if (k < 4) k = 6;
+            if (speed < 0.2f) speed = 0.2f;
 
             pressedImages[i].gameObject.SetActive(true);
             pressedImages[k].gameObject.SetActive(true);
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(speed);
             pressedImages[i].gameObject.SetActive(false);
             pressedImages[k].gameObject.SetActive(false);
 
+            speed -= 0.005f;
             i++;
             k--;
         }
